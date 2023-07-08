d38ec60e13e4f5afc8479ef7845b67bec5d273ef
Parinya9625 <parinya9625@hotmail.com>
2023-07-08 17:22:53 +0700

    Add firstWhereOrNull function

---
+++ lib/src/extensions/lists/search.dart
@@ -0,0 +1,8 @@
+extension SearchListExtension<T> on List<T> {
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (T element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
+}
