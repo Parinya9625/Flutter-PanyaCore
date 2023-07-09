9b02ccb28d224cf3de4f8bd13f83fe8ffc50519f
Parinya9625 <parinya9625@hotmail.com>
2023-07-09 21:32:28 +0700

    Add basic dart doc to StickyHeader

--- lib/src/widgets/sticky_header/sticky_header.dart
+++ lib/src/widgets/sticky_header/sticky_header.dart
@@ -7,15 +7,32 @@ import 'package:flutter/rendering.dart';
 part 'render_sticky_header.dart';
 part 'sticky_header_data.dart';
 
+/// The `data` object provides useful data for handle [StickyHeader]
 typedef StickyHeaderCallback = void Function(StickyHeaderData data);
 
+/// Widget that will pinned header on top when scroll and will
+/// hide header when scroll to last position of child
 class StickyHeader extends MultiChildRenderObjectWidget {
+  /// Widget that will show before [child]
   final Widget header;
+
+  /// Widget that will show after [header]
   final Widget child;
+
+  /// Control how [header] will be sticky in scroll widget
   final bool sticky;
+
+  /// Control how [child] will be render
+  ///
+  /// if `true` : [child] will be render underneath [header] <br>
+  /// if `false` : [child] will be render next to [header]
   final bool overlapHeader;
+
+  /// Function that will call when widget have scroll update or repaint
   final StickyHeaderCallback? callback;
 
+  /// Create Widget that will pinned header on top when scroll and will
+  /// hide header when scroll to last position of child
   StickyHeader({
     super.key,
     required this.header,
--- lib/src/widgets/sticky_header/sticky_header_data.dart
+++ lib/src/widgets/sticky_header/sticky_header_data.dart
@@ -1,12 +1,30 @@
 part of 'sticky_header.dart';
 
+/// [StickyHeader] latest data that will get from [StickyHeader.callback]
 class StickyHeaderData {
+  /// [StickyHeader.header] size
   final Size headerSize;
+
+  /// [StickyHeader.child] size
   final Size childSize;
+
+  /// Indicate the visibility progress of [StickyHeader] widget
+  /// return 0 when widget position at top of the scrolling widget and
+  /// will increase to 1 when scroll to the end of widget and widget no
+  /// longer visible
   final double progress;
+
+  /// Indicate the visibility progress of [StickyHeader.child] widget
+  /// return 0 when [StickyHeader.child] position at top of the scrolling
+  /// widget and will increase to 1 when [StickyHeader.child] no longer visible
+  /// (but [StickyHeader.header] still shown)
   final double childProgress;
+
+  /// Indicate that [StickyHeader.child] is underneath [StickyHeader.header]
+  /// and widget start scrolling
   final bool isOverlaps;
 
+  /// [StickyHeader] latest data that will get from [StickyHeader.callback]
   StickyHeaderData({
     this.headerSize = Size.zero,
     this.childSize = Size.zero,
